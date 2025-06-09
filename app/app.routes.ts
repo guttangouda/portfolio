95d8a75f9e9ee6e75266df243d3ef3a7e0a58856
guttan gouda <guttangouda0@gmail.com>
2025-06-09 22:54:03 +0530

    little code change

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -5,7 +5,8 @@ import {AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { SkillsComponent } from './skills/skills.component'
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'skills', component: SkillsComponent },
--- app/skills/skills.component.ts
+++ app/skills/skills.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LayoutComponent } from "../layout/layout.component";
+import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
 @Component({
   selector: 'app-skills',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.scss'
+  styleUrl: './skills.component.scss',
+  animations: [
+    trigger('skillAnimation', [
+      transition(':enter', [
+        query(
+          '.skill',
+          [
+            style({ opacity: 0, transform: 'scale(0.8)' }),
+            stagger(100, [
+              animate(
+                '400ms ease-out',
+                style({ opacity: 1, transform: 'scale(1)' })
+              )
+            ])
+          ],
+          { optional: true }
+        )
+      ])
+    ])
+  ]
 })
 
 export class SkillsComponent {
