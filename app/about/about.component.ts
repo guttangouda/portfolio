bac0ed851013916c88f47a9d7879c9b3fc4ec805
guttan gouda <guttangouda0@gmail.com>
2025-06-09 13:44:48 +0530

    Update UI

--- app/about/about.component.ts
+++ app/about/about.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 import { LayoutComponent } from "../layout/layout.component";
+import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
+  animations: [
+    trigger('fadeInStagger', [
+      transition(':enter', [
+        query('h2, p, .details div, .download-btn', [
+          style({ opacity: 0, transform: 'translateY(20px)' }),
+          stagger(150, [
+            animate('600ms ease-out', style({ opacity: 1, transform: 'translateY(0)' }))
+          ])
+        ])
+      ])
+    ])
+  ]
 })
 export class AboutComponent {
 
